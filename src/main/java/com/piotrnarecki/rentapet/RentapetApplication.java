ea1a24d7f7a8f32125432b152b3aa66e65b8e794
Piotr Narecki <nareckipiotraleksander@gmail.com>
2024-02-09 11:05:33 +0100

    Initial commit

---
+++ src/main/java/com/piotrnarecki/rentapet/RentapetApplication.java
@@ -0,0 +1,13 @@
+package com.piotrnarecki.rentapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentapetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentapetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piotrnarecki/rentapet/RentapetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrnarecki.rentapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentapetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
