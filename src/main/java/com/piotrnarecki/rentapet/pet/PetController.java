ab12d29bc97161270157354db33857785e28dbd6
Piotr Narecki <nareckipiotraleksander@gmail.com>
2024-02-09 11:30:16 +0100

    more pets added

--- src/main/java/com/piotrnarecki/rentapet/pet/PetController.java
+++ src/main/java/com/piotrnarecki/rentapet/pet/PetController.java
@@ -4,15 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequestMapping(path = "/api/v1/pet")
 
 public class PetController {
-
 
     private final PetService petService;
 
--- src/main/java/com/piotrnarecki/rentapet/pet/PetService.java
+++ src/main/java/com/piotrnarecki/rentapet/pet/PetService.java
@@ -20,14 +20,38 @@ public class PetService {
 
 
         Pet p1 = new Pet(1l, "Surfer", "dog", "Golden Retriver", 100.0);
-        Pet p2 = new Pet(2l, "Schroedinger", "cat", "American Shorthair", 90.0);
-        Pet p3 = new Pet(3l, "Zbigniew", "other", "Pangular", 42.0);
+        Pet p2 = new Pet(2l, "Major", "dog", "Schnauzer", 80.0);
+        Pet p3 = new Pet(3l, "Beag", "dog", "Beagle", 90.0);
+        Pet p4 = new Pet(4l, "Scoobie Doo", "dog", "Canarian Dog", 180.0);
+
+
+        Pet p5 = new Pet(5l, "Schroedinger", "cat", "American Shorthair", 90.0);
+        Pet p6 = new Pet(6l, "Garfield", "cat", "Orange Tabby", 110.0);
+        Pet p7 = new Pet(7l, "Laura", "cat", "Scottish Fold", 100.0);
+        Pet p8 = new Pet(8l, "Gandalf", "cat", "American Shorthair", 100.0);
+
+
+        Pet p9 = new Pet(9l, "Zbigniew", "other", "Pangular", 42.0);
+        Pet p10 = new Pet(10l, "Linus", "other", "Linux", 42.0);
+        Pet p11 = new Pet(11l, "Rider", "other", "dBeaver", 42.0);
+        Pet p12 = new Pet(12l, "Guido", "other", "Python", 42.0);
+
+
 
         List<Pet> petsList = new ArrayList<>();
 
         petsList.add(p1);
         petsList.add(p2);
         petsList.add(p3);
+        petsList.add(p4);
+        petsList.add(p5);
+        petsList.add(p6);
+        petsList.add(p7);
+        petsList.add(p8);
+        petsList.add(p9);
+        petsList.add(p10);
+        petsList.add(p11);
+        petsList.add(p12);
 
         return petsList;
 
